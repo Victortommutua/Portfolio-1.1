84ee017d1d4bf106644f58b07fbba66eec1a5b0c
victortommutua <mutuavictor801@gmail.com>
2022-10-10 22:36:30 +0300

    portfoili-1.1

---
+++ components/Skill.tsx
@@ -0,0 +1,36 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+import { urlFor } from '../sanity';
+import { Skill } from '../typings';
+
+type Props = {
+    skill: Skill;
+    directionLeft?: boolean;
+}
+
+function Skill({skill, directionLeft}: Props) {
+  return (
+    <div className='group relative flex cursor-pointer'>
+        <motion.img
+        initial={{
+            x: directionLeft ? -200: 200,
+            opacity: 0,
+        }}
+        whileInView={{ opacity: 1, x: 0}}
+        transition={{duration: 1, }}
+        src={urlFor(skill?.image).url()}
+        className='rounded-full border border-gray-500 object-cover h-10 w-10
+        xl:w-20 xl:h-20 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition 
+        duration-300 ease-in-out group-hover:bg-white h-10 w-10 md:w-28 md:h-28
+        xl:w-32 xl:h-32 rounded-full z-0'>
+            <div className='flex items-center justify-center h-full'>
+                <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skill
